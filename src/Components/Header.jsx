7442480d1aaf0c7dea84747b7f263d6df1a02317
marzonha <marzonhapacheco@gmail>
2024-06-05 17:30:53 -0300

    Foi colocado a Tag Link
    para se usar o react  de forma completa, usamos a tag "Link", já a tag "a" usa de forma parcial o react - 05/06/24

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,15 +6,34 @@ export function Header() {
   return (
     <header className={styles.header}>
       <ul className={styles.menu}>
-        <li><a href="./Home">Home</a></li>
-        <li><a href="./Mission">Missão</a></li>
-        <li><a href="./Product">Produto</a></li>
-        <li><a href="./History">História</a></li>
-        <li><a href="./Contact">Contato</a></li>        
+        <li><Link to="./">Home</Link></li>
+        <li><Link to="./Mission">Missão</Link></li>
+        <li><Link to="./Product">Produtos</Link></li>
+        <li><Link to="./History">Nossa História</Link></li>
+        <li><Link to="./Contact">Contato</Link></li>        
       </ul>
     </header>
 
 
   )
 }
+
+  // Exemplo com a Tag "a"
+
+// export function Header() {
+//   return (
+//     <header className={styles.header}>
+//       <ul className={styles.menu}>
+//         <li><a href="./Home">Home</a></li>
+//         <li><a href="./Mission">Missão</a></li>
+//         <li><a href="./Product">Produto</a></li>
+//         <li><a href="./History">História</a></li>
+//         <li><a href="./Contact">Contato</a></li>        
+//       </ul>
+//     </header>
+
+
+//   )
+// }
+
 

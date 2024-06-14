01998b97c7f682bdc7da093f97b6f8d976c1d8d4
marzonha <marzonhapacheco@gmail>
2024-06-14 15:07:35 -0300

    Quase Completo a Pagina

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -1,9 +1,13 @@
+import {MissionContent} from './MissionContent';
 
  function Mission(props) {
   return (
     <div>
         <h1>{props.nomePagina}</h1>
         <p>{props.paginaPrincipal}</p>
+
+        <MissionContent/>
+        
     </div>
   )
 }
